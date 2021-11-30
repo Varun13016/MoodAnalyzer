9fcc00c1fd79d1ea09976f6971b2b65e64182b1d
“Varun13016” <dmevarun@gmail.com>
2021-11-30 12:29:01 +0530

    Add project files.

---
+++ MoodAnalyzer-Test/UnitTest1.cs
@@ -0,0 +1,35 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyzerProgram;
+
+namespace MoodAnalyzer_Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void msgSadMood()
+        {
+            string msg = "I am in sad mood";
+            string expected = "SAD";
+
+            MoodAnalyzer mood = new MoodAnalyzer(msg);
+
+            string actual = mood.AnalyseMood();
+
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void msgHaapyMood()
+        {
+            string msg = "I am in Any mood";
+            string expected = "HAPPY";
+
+            MoodAnalyzer mood = new MoodAnalyzer(msg);
+
+            string actual = mood.AnalyseMood();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
