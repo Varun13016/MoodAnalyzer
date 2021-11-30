713a4311fda624c25e902b26442c3c1bff3aad1c
“Varun13016” <dmevarun@gmail.com>
2021-11-30 12:32:34 +0530

    UC1

--- MoodAnalyzer/MoodAnalyzer.cs
+++ MoodAnalyzer/MoodAnalyzer.cs
@@ -7,12 +7,8 @@ namespace MoodAnalyzerProgram
     public class MoodAnalyzer
     {
         public string msg;
-        public MoodAnalyzer(string msg)
-        {
-            this.msg = msg;
-        }
-
-        public string AnalyseMood()
+       
+        public string AnalyseMood(string msg)
         {
             if (msg.ToLower().Contains("sad"))
                 return "SAD";
